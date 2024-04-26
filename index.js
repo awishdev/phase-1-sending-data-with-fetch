f11412e37722aa868bdb69fdaf6b18cdf0ee5169
Arthur Ish <arthurishdev@gmail.com>
2024-04-25 18:27:08 -0700

    posted!

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Add your code here
+function submitData(userName, userEmail){
+    const userData = {
+        name: `${userName}`,
+        email: `${userEmail}`,
+      };
+      
+      const jsonPOST = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json",
+        },
+        body: JSON.stringify(userData),
+      };
+      
+      return fetch("http://localhost:3000/users", jsonPOST)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (object) {
+            const p = document.createElement("p");
+            const t = document.createTextNode(`${object.id}`)
+            p.appendChild(t);
+            
+            document.body.appendChild(p);
+        })
+        .catch(function (error) {
+
+          const p = document.createElement("p");
+          const t = document.createTextNode(`${error.message}`)
+          p.appendChild(t);
+          document.body.appendChild(p);
+
+          console.log(error.message);
+        });
+
+}
